7cfe74f7f4810387b410e69f49f1f2d2cec5a0aa
David <david_pogosian@outlook.com>
2023-09-17 14:18:13 -0400

    added navbar w/ 4 base components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 //import logo from './logo.svg';
 //import './App.css';
 
+// Beau has react imported here... suspicious
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
+// browser said to add <Routes>!
+// change component to element
+// wrap components in </>
 function App() {
   return (
-    <div className="container">
-      Hello World!
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Routes>
+          <Route path="/" exact element={<ExercisesList/>} />
+          <Route path="/edit/:id" element={<EditExercise/>} />
+          <Route path="/create" element={<CreateExercise/>} />
+          <Route path="/user" element={<CreateUser/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
